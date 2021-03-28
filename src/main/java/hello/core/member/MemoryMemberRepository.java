f61ee3c824bed8540a96410808c35411b7cf55cc
yjh <korvata@naver.com>
2021-03-28 23:06:30 +0900

    first commit

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,24 @@
+package hello.core.member;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class MemoryMemberRepository implements MemberRepository{
+
+    //실무에서는 동시 접근 이슈 때문에 concurrentHashMap 사용
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
---
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,49 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
+        bean1.addCount();
+        assertThat(bean1.getCount()).isEqualTo(1);
+
+        PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
+        bean2.addCount();
+        assertThat(bean2.getCount()).isEqualTo(1);
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean{
+        private int count = 0;
+
+        public void addCount(){
+            count++;
+        }
+
+        public int getCount(){
+            return count;
+        }
+
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.destroy " + this);
+        }
+    }
+}
